2e2ec11f660f6947ba9f5e885cf4e844aed31bef
898444038@qq.com <898444038@qq.com>
2020-11-11 18:36:29 +0800

    commit

--- src/views/config/configEquip.vue
+++ src/views/config/configEquip.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <vx-card>
-      <vxe-form :data="queryData" @submit="getData">
-        <vxe-form-item title="名称" field="name" :title-prefix="{ message: '抽奖名称' }">
+      <vxe-form :data="queryData">
+        <vxe-form-item title="名称" field="name" :title-prefix="{ message: '装备名称' }">
           <template v-slot>
-            <vxe-input v-model="queryData.name" placeholder="请输入名称"></vxe-input>
+            <vs-input v-model="queryData.name" placeholder="请输入名称"></vs-input>
+          </template>
+        </vxe-form-item>
+        <vxe-form-item title="类型" field="type" :title-prefix="{ message: '装备类型' }" style="width: 240px;">
+          <template v-slot>
+            <v-select label="name" placeholder='请选择类型' v-model='queryData.type' :options="typeOptions" :dir="$vs.rtl ? 'rtl' : 'ltr'" />
           </template>
         </vxe-form-item>
         <vxe-form-item>
           <template v-slot>
-            <vxe-button type="submit" status="primary">查询</vxe-button>
-            <vxe-button type="reset">重置</vxe-button>
+            <vs-button type="relief" class="mr-3 mb-2 centery-1" @click="getData">查询</vs-button>
+            <vs-button type="relief" color="warning" class="mb-2" @click="resetForm">重置</vs-button>
           </template>
         </vxe-form-item>
       </vxe-form>
@@ -59,7 +64,8 @@
             <vxe-table-column type="seq"  title="序号" width="60"></vxe-table-column>
             <!-- <vxe-table-column type="radio" width="60"></vxe-table-column>
             <vxe-table-column type="checkbox" width="60" style="padding: 0;"></vxe-table-column> -->
-            <vxe-table-column field="name" title="名称" :title-help="{message: '抽奖名称'}"></vxe-table-column>
+            <vxe-table-column field="name" title="名称" :title-help="{message: '装备名称'}"></vxe-table-column>
+            <vxe-table-column field="type" title="类型" :formatter="formatterType" :title-help="{message: '装备类型'}"></vxe-table-column>
             <!-- <vxe-table-column field="age" title="Age" formatter="formatAmount" sortable></vxe-table-column>
             <vxe-table-column field="sex" title="Sex" formatter="formatSex" :filters="[{value:'0',label:'女'},{value:'1',label:'男'}]"></vxe-table-column>
             <vxe-table-column field="sex2" title="Sex2" :formatter="['formatDate', 'yyyy-MM-dd']" sortable :filters="[{value:'0',label:'女'},{value:'1',label:'男'}]" :filter-multiple="false"></vxe-table-column>
@@ -149,15 +155,23 @@
 <script>
 import { VueLoading } from 'vue-loading-template'
 import { SlickList, SlickItem } from 'vue-slicksort'
+import vSelect from 'vue-select'
 
 export default {
   components: {
-    VueLoading,
+    'vue-loading':VueLoading,
     SlickItem,
-    SlickList
+    SlickList,
+    'v-select': vSelect,
   },
   data() {
     return {
+      typeOptions:[],
+      pageUrl: '/equip/selectPage',
+      insertUrl: '/equip/insert',
+      updateUrl: '/equip/update',
+      deleteUrl: '',
+
       settings: false,
       rules: false,
       loading: false,
@@ -166,39 +180,29 @@ export default {
       filterName: '',//搜索
       size: "medium",//medium / small / mini
       allAlign: 'left',//left居左,center居中,right居右
-      queryData: {
-        name: '',
-      },
-      formData: {
-        id: '',
-        name: '',
-      },
-      tableData: [],
+      queryData: {},//查询表单
+      formData: {},//表单数据
+      tableData: [],//列表数据
       itemData: [],
       tablePage: {
         currentPage: 1,
         pageSize: 10,
         totalResult: 0
       },
-      sexList: [
-        {label: '女',value: '0'},
-        {label: '男',value: '1'}
-      ],
+      typeList: [],
       formRules: {
         name: [
           { required: true, message: '请输入名称' },
           { min: 2, max: 30, message: '长度在 2 到 30 个字符' }
         ],
-        nickname: [
-          { required: true, message: '请输入昵称' }
-        ],
-        sex: [
-          { required: true, message: '请选择性别' }
+        type: [
+          { required: true, message: '请选择类型' }
         ]
       },
       formItems: [
         { field: 'name', title: '名称', span: 12, itemRender: { name: '$input', props: { placeholder: '请输入名称' } } },
-
+        //{ field: 'type', title: '类型', span: 12, itemRender: { name: '$input', props: { placeholder: '请输入类型' } } },
+        { field: 'type', title: '类型', span: 12, itemRender: { name: '$select', options: [] } },
         { align: 'center', span: 24, titleAlign: 'left', itemRender: { name: '$buttons', children: [{ props: { type: 'submit', content: '提交', status: 'primary' } }, { props: { type: 'reset', content: '重置' } }] } }
       ],
       items: [
@@ -210,7 +214,10 @@ export default {
     this.getData();
   },
   mounted() {
-    
+    this.getTypeList();
+    var initData = this.getFormData();
+    this.formData = initData;
+    this.queryData = initData;
   },
   watch: {
     
@@ -219,33 +226,162 @@ export default {
     
   },
   methods: {
-    addLuckItem(){
-      this.items.push({name:'',weight:'',remark:''});
-    },
-    removeLuckItem(index){
-      if(this.items.length <=1){
-        this.$XModal.message({ message: '无法继续删除',zIndex:222222 })
-        return;
+    resetForm(){
+      this.queryData = {
+        name: '',
+        type: ''
       }
-      this.items.splice(index,1);
     },
-    submitLuckItem(){
+    getFormData(row){
+      if(row){
+        return {
+          id: row.id,
+          name: row.name,
+          type: row.type,
+        }
+      }
+      return {
+        id: '',
+        name: '',
+        type: ''
+      }
+    },
+    getQueryParams(){
+      let obj = new Object();
+      obj.pageNo = this.tablePage.currentPage;
+      obj.pageSize = this.tablePage.pageSize;
+      obj.name = this.queryData.name;
+      if(this.queryData.type){
+        obj.type = this.queryData.type.code;
+      }
+      return obj;
+    },  
+    getTypeList(){
+        var _this = this;
+        _this.$https.get("/equip/type/selectList",{}).then((response) => { 
+            if(response.code == 1){
+                _this.typeOptions = response.data;
+                let typeList = [];
+                for(let i=0;i<_this.typeOptions.length;i++){
+                  typeList.push({label: _this.typeOptions[i].name,value: _this.typeOptions[i].code},);
+                }
+                _this.typeList = typeList;
+                _this.formItems[1].itemRender.options = _this.typeList;
+            }else{
+                _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){} });
+            }
+        }).catch((error) => { console.log("error",error) });
+    },
+    formatterType ({ cellValue }) {
+      let item = this.typeList.find(item => item.value === cellValue)
+      return item ? item.label : ''
+    },
+
+    // addLuckItem(){
+    //   this.items.push({name:'',weight:'',remark:''});
+    // },
+    // removeLuckItem(index){
+    //   if(this.items.length <=1){
+    //     this.$XModal.message({ message: '无法继续删除',zIndex:222222 })
+    //     return;
+    //   }
+    //   this.items.splice(index,1);
+    // },
+    // submitLuckItem(){
+    //   var _this = this;
+    //   _this.loading = true;
+    //   var params = {
+    //     id: this.formData.id,
+    //     items: JSON.stringify(this.items)
+    //   }
+    //   _this.$https.post("/luck/update",params).then((response) => { 
+    //     console.log("luck update response",response);
+    //     if(response.code == 1){
+    //         _this.settings = false;
+    //         _this.$vs.dialog({color: 'success',title: '成功',text: "更新成功",accept: function(){}});
+    //     }else{
+    //         _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}});
+    //     }
+    //     _this.loading = false;
+    //   }).catch((error) => { console.log(error) });
+    // },
+    
+    
+    
+    
+    // settingsItem(row){
+    //   this.formData = {
+    //     id: row.id,
+    //   }
+    //   this.settings = true
+    //   this.items = JSON.parse(row.items);
+    //   if(!this.items){
+    //     this.items = []
+    //   }
+    // },
+
+
+    // removeItem (row) {
+    //   this.$XModal.confirm('您确定要删除该数据?').then(type => {
+    //     if (type === 'confirm') {
+    //       this.$refs.xTable.remove(row)
+    //     }
+    //   })
+    // },
+    getData(){
       var _this = this;
+      var queryParams = _this.getQueryParams();
       _this.loading = true;
-      var params = {
-        id: this.formData.id,
-        items: JSON.stringify(this.items)
-      }
-      _this.$https.post("/luck/update",params).then((response) => { 
-        console.log("luck update response",response);
+      _this.$https.get(_this.pageUrl ,queryParams).then((response) => { 
+        console.log(_this.pageUrl,response);
         if(response.code == 1){
-            _this.settings = false;
-            _this.$vs.dialog({color: 'success',title: '成功',text: "更新成功",accept: function(){}});
+          _this.tableData = response.data.data;
+          _this.tablePage.totalResult = response.data.totalPage;
         }else{
             _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}});
         }
         _this.loading = false;
-      }).catch((error) => { console.log(error) });
+      }).catch((error) => { _this.loading = false;console.log(error) });
+    },
+    insertItem () {
+      this.formData = this.getFormData();
+      this.selectRow = null
+      this.showEdit = true
+    },
+    editItem (row) {
+      this.formData = this.getFormData(row);
+      this.selectRow = row
+      this.showEdit = true
+    },
+    submitItem () {
+      var _this = this;
+      _this.loading = true
+      if (_this.selectRow) {
+        _this.$https.post(_this.updateUrl, _this.formData).then((response) => { 
+          console.log(_this.updateUrl,response)
+          if(response.code == 1){
+            _this.$XModal.message({ message: '保存成功', status: 'success' })
+            Object.assign(_this.selectRow, _this.formData)
+          }else{
+            _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}})
+          }
+          _this.loading = false
+          _this.showEdit = false
+        }).catch((error) => { _this.loading = false;console.log(error) })
+      } else {
+        _this.$https.post(_this.insertUrl, _this.formData).then((response) => { 
+          console.log(_this.insertUrl,response)
+          if(response.code == 1){
+            _this.$XModal.message({ message: '新增成功', status: 'success' })
+            _this.formData.id = response.data;
+            _this.$refs.xTable.insert(_this.formData)
+          }else{
+            _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}})
+          }
+          _this.loading = false
+          _this.showEdit = false
+        }).catch((error) => { _this.loading = false;console.log(error) })
+      }
     },
     // 导出
     exportDataEvent () {
@@ -259,95 +395,17 @@ export default {
       this.tablePage.pageSize = pageSize
       this.getData()
     },
-    getData(){
-      var _this = this;
-      _this.loading = true;
-      var queryParams = {
-        pageNo: _this.tablePage.currentPage,
-        pageSize: _this.tablePage.pageSize,
-        name: _this.queryData.name
-      }
-      _this.$https.get("/luck/selectPage",queryParams).then((response) => { 
-        console.log("luck selectPage response",response);
-        if(response.code == 1){
-          _this.tableData = response.data.data;
-          _this.tablePage.totalResult = response.data.totalPage;
-          _this.loading = false;
-        }else{
-            _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}});
-        }
-      }).catch((error) => { console.log(error) });
-    },
-    insertItem () {
-      this.formData = {
-        id: '',
-        name: '',
-      }
-      this.selectRow = null
-      this.showEdit = true
-    },
-    editItem (row) {
-      this.formData = {
-        id: row.id,
-        name: row.name,
-      }
-      this.selectRow = row
-      this.showEdit = true
-    },
-    removeItem (row) {
-      this.$XModal.confirm('您确定要删除该数据?').then(type => {
-        if (type === 'confirm') {
-          this.$refs.xTable.remove(row)
-        }
-      })
-    },
-    settingsItem(row){
-      this.formData = {
-        id: row.id,
-      }
-      this.settings = true
-      this.items = JSON.parse(row.items);
-      if(!this.items){
-        this.items = []
-      }
-    },
-    submitItem () {
-      var _this = this;
-      _this.loading = true
-      if (_this.selectRow) {
-        _this.$https.post("/luck/update", _this.formData).then((response) => { 
-          console.log("luck update response",response)
-          if(response.code == 1){
-            _this.$XModal.message({ message: '保存成功', status: 'success' })
-            Object.assign(_this.selectRow, _this.formData)
-          }else{
-            _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}})
-          }
-          _this.loading = false
-          _this.showEdit = false
-        }).catch((error) => { console.log(error) })
-      } else {
-        _this.$https.post("/luck/insert", _this.formData).then((response) => { 
-          console.log("luck insert response",response)
-          if(response.code == 1){
-            _this.$XModal.message({ message: '新增成功', status: 'success' })
-            _this.formData.id = response.data;
-            _this.$refs.xTable.insert(_this.formData)
-          }else{
-            _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){}})
-          }
-          _this.loading = false
-          _this.showEdit = false
-        }).catch((error) => { console.log(error) })
-      }
-    }
   },
+  
 }
 
 </script>
-
+<style>
+.vxe-select--panel.is--transfer{ z-index: 222223!important;}
+</style>
 <style scoped>
 .vxe-modal--wrapper{ z-index: 100001!important;}
+.vxe-select-option--wrapper,.vxe-select--panel,.vxe-select--panel.is--transfer{ z-index: 222223!important;}
 .vs-dropdown--item{
   width: 110px;
 }
@@ -360,6 +418,7 @@ export default {
   display: inline-block;
   padding: 0 4px 0 4px;
 }
+
 .list-item{
   z-index: 222222;
   /* border: 1px solid rgb(238, 238, 238); */
