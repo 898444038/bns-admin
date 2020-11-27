489d46e5b00577e1e5e2a0a44882fa50a4db1463
898444038@qq.com <898444038@qq.com>
2020-11-27 18:37:21 +0800

    commit

--- src/views/tools/equip/equipGrow.vue
+++ src/views/tools/equip/equipGrow.vue
@@ -54,8 +54,45 @@
                 <div v-for="(item,index) in routes" :key="index" class="vx-row" style="margin: 20px 0 0 0;width: 100%;">
                   <vs-divider color="primary"> 成长路线{{index+1}} </vs-divider>
                   <span v-for="(list,index2) in routes[index].list" :key="index2"> -> {{list.name}}</span>
-                  <div class="vx-row" style="width: 100%;margin: 0;">1段-2段</div>
-                  <div class="vx-row" style="width: 100%;margin: 0;">2段-3段</div>
+                  <vxe-table style="width:100%;" class="mytable-footer"
+                    border
+                    resizable
+                    height="300"
+                    highlight-hover-row
+                    :show-footer="showFooter"
+                    :footer-method="footerMethod"
+                    :footer-cell-class-name="footerCellClassName3"
+                    :data="tableData">
+                    <vxe-table-column type="seq" width="60" fixed="left"></vxe-table-column>
+                    <vxe-table-column v-for="(item,index) in 10" :key="index" field="name" title="Name" min-width="600" sortable></vxe-table-column>
+                    <vxe-table-column field="age" title="Age" min-width="600"></vxe-table-column>
+                    <vxe-table-column field="date" title="Date" min-width="600"></vxe-table-column>
+                    <vxe-table-column field="rate" title="Rate" width="200" fixed="right"></vxe-table-column>
+                  </vxe-table>
+                  <!-- <table>
+                    <thead>
+                      <tr>
+                        <th>支付</th>
+                        <th>日期</th>
+                        <th>金额</th>
+                        <th>周期</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td data-label="支付">支付 #1</td>
+                        <td data-label="日期">02/01/2015</td>
+                        <td data-label="金额">￥2,311</td>
+                        <td data-label="周期">01/01/2015 - 01/31/2015</td>
+                      </tr>
+                      <tr>
+                        <td data-label="支付">支付 #2</td>
+                        <td data-label="日期">03/01/2015</td>
+                        <td data-label="金额">￥3,211</td>
+                        <td data-label="周期">02/01/2015 - 02/28/2015</td>
+                      </tr>
+                    </tbody>
+                  </table> -->
                 </div>
             </vs-tab>
         </vs-tabs>
@@ -66,6 +103,8 @@
 <script>
 import StatisticsCardLine from '@/components/statistics-cards/StatisticsCardLine.vue'
 import vSelect from 'vue-select'
+import XEUtils from 'xe-utils'
+
 export default {
   components: {
     'v-select': vSelect,
@@ -81,7 +120,18 @@ export default {
       endOptions:[],
       end:'',
       materials:[],
-      routes:[]
+      routes:[],
+      showFooter: true,
+      tableData: [
+        { id: 10001, name: 'Test1', role: 'Develop', sex: '0', age: 28, amount: 888, address: 'vxe-table 从入门到放弃' },
+        { id: 10002, name: 'Test2', role: 'Test', sex: '1', age: 22, amount: 666, address: 'Guangzhou' },
+        { id: 10003, name: 'Test3', role: 'PM', sex: '1', age: 32, amount: 89, address: 'Shanghai' },
+        { id: 10004, name: 'Test4', role: 'Designer', sex: '0', age: 23, amount: 1000, address: 'vxe-table 从入门到放弃' },
+        { id: 10005, name: 'Test5', role: 'Develop', sex: '0', age: 30, amount: 999, address: 'Shanghai' },
+        { id: 10006, name: 'Test6', role: 'Designer', sex: '0', age: 21, amount: 998, address: 'vxe-table 从入门到放弃' },
+        { id: 10007, name: 'Test7', role: 'Test', sex: '1', age: 29, amount: 2000, address: 'vxe-table 从入门到放弃' },
+        { id: 10008, name: 'Test8', role: 'Develop', sex: '1 ', age: 35, amount: 999, address: 'vxe-table 从入门到放弃' }
+      ]
     }
   },
   watch: {
@@ -148,6 +198,50 @@ export default {
               _this.$vs.dialog({color: 'danger',title: '警告',text: response.msg,accept: function(){} });
           }
       }).catch((error) => { console.log("error",error) });
+    },
+    footerCellClassName3 ({ $rowIndex, column }) {
+      if (column.type === 'seq') {
+        if ($rowIndex === 0) {
+          return 'col-blue'
+        } else {
+          return 'col-red'
+        }
+      } else if (column.property === 'age') {
+        if ($rowIndex === 1) {
+          return 'col-red'
+        }
+      }
+    },
+    footerMethod ({ columns, data }) {
+      const means = []
+      const sums = []
+      const others = []
+      columns.forEach((column, columnIndex) => {
+        if (columnIndex === 0) {
+          means.push('平均')
+          sums.push('和值')
+          others.push('其他')
+        } else {
+          let meanCell = null
+          let sumCell = null
+          let otherCell = '-'
+          switch (column.property) {
+            case 'age':
+            case 'amount':
+              meanCell = parseInt(XEUtils.mean(data, column.property))
+              sumCell = XEUtils.sum(data, column.property)
+              break
+            case 'sex':
+              otherCell = '无'
+              break
+          }
+          means.push(meanCell)
+          sums.push(sumCell)
+          others.push(otherCell)
+        }
+      })
+      // 返回一个二维数组的表尾合计
+      return [means, sums, others]
     }
   },
   mounted() {
@@ -170,4 +264,77 @@ export default {
 .word-td{Word-break: break-all;}
 .vs-tabs--content{margin-top: 10px;    min-height: 450px;}
 .v-select ul{height:200px}
+
+
+.mytable-footer .col-blue {
+  background-color: #2db7f5;
+  color: #fff;
+}
+.mytable-footer .col-red {
+  background-color: red;
+  color: #fff;
+}
+
+
+
+table {
+    border: 1px solid #ccc;
+    width: 100%;
+    margin:0;
+    padding:0;
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin: 0 auto;
+  }
+
+  table tr {
+    border: 1px solid #ddd;
+    padding: 5px;
+  }
+
+  table th, table td {
+    padding: 10px;
+    text-align: center;
+  }
+
+  table th {
+    text-transform: uppercase;
+    font-size: 14px;
+    letter-spacing: 1px;
+  }
+
+  @media screen and (max-width: 600px) {
+
+    table {
+      border: 0;
+    }
+
+    table thead {
+      display: none;
+    }
+
+    table tr {
+      margin-bottom: 10px;
+      display: block;
+      border-bottom: 2px solid #ddd;
+    }
+
+    table td {
+      display: block;
+      text-align: right;
+      font-size: 13px;
+      border-bottom: 1px dotted #ccc;
+    }
+
+    table td:last-child {
+      border-bottom: 0;
+    }
+
+    table td:before {
+      content: attr(data-label);
+      float: left;
+      text-transform: uppercase;
+      font-weight: bold;
+    }
+  }
 </style>
